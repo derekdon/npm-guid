3a24206d590173b5d41b29c05b33b320a3acd215
derekdon <derek@codex9.com>
2016-05-22 23:56:14 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+export default class GUID {
+
+    constructor() {
+    }
+
+    generate() {
+        return this._s4() + this._s4() + '-' + this._s4() + '-' + this._s4() +
+            '-' + this._s4() + '-' + this._s4() + this._s4() + this._s4();
+    }
+
+    _s4() {
+        return Math.floor((1 + Math.random()) * 0x10000)
+            .toString(16).substring(1);
+    }
+}
+
+GUID.factory.$inject = [];
